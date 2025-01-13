e9532ea96e868bc6cf0a9a3a3fba7249c070ac9b
adigaboy <56532882+adigaboy@users.noreply.github.com>
2025-01-13 22:26:02 +0200

    Update mock.py
    adapt mocks to changes

--- tests/mock.py
+++ tests/mock.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import AsyncMock
 
 tested_html = '''
 <table>
@@ -19,34 +19,83 @@ tested_html = '''
     <th>Collateral adjective</th>
   </tr>
   <tr>
-    <td>Bat</td>
+    <td><a href="/wiki/Bat" title="Bat">Bat</a></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
     <td>noctillionine pteropine</td>
   </tr>
   <tr>
-    <td>Bear</td>
+    <td><a href="/wiki/Bear" title="Bear">Bear</a></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
     <td>ursine</td>
   </tr>
   <tr>
-    <td>Polar Bear</td>
+    <td><a href="/wiki/Polar Bear" title="Polar Bear">Polar Bear</a></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
     <td>ursine</td>
   </tr>
   <tr>
-    <td>Ape</td>
+    <td><a href="/wiki/Ape" title="Ape">Ape</a></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
     <td>simian</td>
   </tr>
   <tr>
-    <td>Gorilla</td>
+    <td><a href="/wiki/Gorilla" title="Gorilla">Gorilla</a></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
     <td>simian</td>
   </tr>
   <tr>
-    <td>Human</td>
+    <td><a href="/wiki/Human" title="Human">Human</a></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
     <td>simian</td>
   </tr>
 </table>
 '''
 
+async def mock_download_images(*args, **kwargs):
+    return args[1:]
 
-def mock_page_response(*args):
-    mock_page = MagicMock()
-    mock_page.html = MagicMock(return_value=tested_html)
-    return mock_page
+class aiohttp_clientsession_mock(AsyncMock):
+    status = 200
+    def __init__(self):
+        pass
+    def get(self, *args, **kwargs):
+        return base_response_class()
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+
+class base_response_class(AsyncMock):
+    status = 200
+    def __init__(self):
+        pass
+    def get(self, *args, **kwargs):
+        pass
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+    async def text(self):
+        return tested_html
